70f3e5fbf87132f5ef6886ce21431ee07364462f
ZSteiger <z.steig93@gmail.com>
2019-03-27 13:20:54 -0400

    Reorganized photos

--- photos.js
+++ photos.js
@@ -1,10 +1,24 @@
 const bhutanPhotos = [
   {
     src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135165/Folck%20Photo/Bhutan/Trees_Prayers.jpg',
-    width: 36,
-    height: 52,
-    alt: 'Trees, Prayers'
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/Butterlamp_Alchemist.jpg',
+    width: 32,
+    height: 49,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/Along_a_Mountain_Road.jpg',
+    width: 32,
+    height: 49,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/Bearing_Rice_Up_a_Hillside.jpg',
+    width: 32,
+    height: 49,
+    alt: ''
   },
   {
     src:
@@ -15,30 +29,37 @@ const bhutanPhotos = [
   },
   {
     src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135165/Folck%20Photo/Bhutan/Tashi_s_Prayers_and_Mine.jpg',
-    width: 23,
-    height: 32,
-    alt: "Tashi's Prayers and Mine"
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135165/Folck%20Photo/Bhutan/Seen_if_only_for_a_moment.jpg',
-    width: 49,
-    height: 32,
-    alt: 'Seen if only for a moment'
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135165/Folck%20Photo/Bhutan/Taktsang_Monastery_Tiger_s_Nest.jpg',
-    width: 14,
-    height: 17,
-    alt: 'Taktsang Monastery Tigers Nest'
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135165/Folck%20Photo/Bhutan/Monks_Beside_a_Fire.jpg',
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/A_Faithful_and_Patient_Man.jpg',
     width: 32,
     height: 49,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/A_Gift.jpg',
+    width: 32,
+    height: 49,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135154/Folck%20Photo/Bhutan/Generational_Shift.jpg',
+    width: 32,
+    height: 23,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135164/Folck%20Photo/Bhutan/Joyful_Morning.jpg',
+    width: 32,
+    height: 49,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135154/Folck%20Photo/Bhutan/Classroom_Reading_Dechenphug_Monastery.jpg',
+    width: 49,
+    height: 32,
     alt: ''
   },
   {
@@ -50,35 +71,7 @@ const bhutanPhotos = [
   },
   {
     src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135164/Folck%20Photo/Bhutan/Trees_within_the_Clouds.jpg',
-    width: 36,
-    height: 55,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135164/Folck%20Photo/Bhutan/Monk_Traversed_a_Smokey_Courtyard.jpg',
-    width: 32,
-    height: 49,
-    alt: ''
-  },
-  {
-    src:
       'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135164/Folck%20Photo/Bhutan/Monk_Sweeping.jpg',
-    width: 32,
-    height: 49,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135164/Folck%20Photo/Bhutan/Monk_in_a_Dark_Room.jpg',
-    width: 32,
-    height: 49,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135164/Folck%20Photo/Bhutan/Joyful_Morning.jpg',
     width: 32,
     height: 49,
     alt: ''
@@ -99,16 +92,58 @@ const bhutanPhotos = [
   },
   {
     src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135154/Folck%20Photo/Bhutan/Classroom_Reading_Dechenphug_Monastery.jpg',
-    width: 49,
-    height: 32,
+      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135164/Folck%20Photo/Bhutan/Monk_in_a_Dark_Room.jpg',
+    width: 32,
+    height: 49,
     alt: ''
   },
   {
     src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135154/Folck%20Photo/Bhutan/Generational_Shift.jpg',
+      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135165/Folck%20Photo/Bhutan/Monks_Beside_a_Fire.jpg',
     width: 32,
-    height: 23,
+    height: 49,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135164/Folck%20Photo/Bhutan/Monk_Traversed_a_Smokey_Courtyard.jpg',
+    width: 32,
+    height: 49,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/Golden_Spires_Chorten_Kora.jpg',
+    width: 32,
+    height: 49,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135165/Folck%20Photo/Bhutan/Tashi_s_Prayers_and_Mine.jpg',
+    width: 23,
+    height: 32,
+    alt: "Tashi's Prayers and Mine"
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/Butterlamps_A_Village_to_be_Seen.jpg',
+    width: 32,
+    height: 49,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135165/Folck%20Photo/Bhutan/Seen_if_only_for_a_moment.jpg',
+    width: 49,
+    height: 32,
+    alt: 'Seen if only for a moment'
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/Butter_Lamps_in_Black_and_White.jpg',
+    width: 17,
+    height: 14,
     alt: ''
   },
   {
@@ -120,9 +155,23 @@ const bhutanPhotos = [
   },
   {
     src:
-      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/Butterlamps_A_Village_to_be_Seen.jpg',
-    width: 32,
-    height: 49,
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/Buddha_Point_Kuensel_Phodreng_with_Prayer_Flags.jpg',
+    width: 18,
+    height: 14,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135165/Folck%20Photo/Bhutan/Trees_Prayers.jpg',
+    width: 36,
+    height: 52,
+    alt: 'Trees, Prayers'
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135164/Folck%20Photo/Bhutan/Trees_within_the_Clouds.jpg',
+    width: 36,
+    height: 55,
     alt: ''
   },
   {
@@ -141,63 +190,28 @@ const bhutanPhotos = [
   },
   {
     src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/Butterlamp_Alchemist.jpg',
-    width: 32,
-    height: 49,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/Bearing_Rice_Up_a_Hillside.jpg',
-    width: 32,
-    height: 49,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/Butter_Lamps_in_Black_and_White.jpg',
-    width: 17,
-    height: 14,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/Along_a_Mountain_Road.jpg',
-    width: 32,
-    height: 49,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/Golden_Spires_Chorten_Kora.jpg',
-    width: 32,
-    height: 49,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/a_auto_left/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/A_Gift.jpg',
-    width: 32,
-    height: 49,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/A_Faithful_and_Patient_Man.jpg',
-    width: 32,
-    height: 49,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135153/Folck%20Photo/Bhutan/Buddha_Point_Kuensel_Phodreng_with_Prayer_Flags.jpg',
-    width: 18,
-    height: 14,
-    alt: ''
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551135165/Folck%20Photo/Bhutan/Taktsang_Monastery_Tiger_s_Nest.jpg',
+    width: 14,
+    height: 17,
+    alt: 'Taktsang Monastery Tigers Nest'
   }
 ];
 
 const studioPhotos = [
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Flora_Brown_I.jpg',
+    width: 15,
+    height: 20,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Rachel_with_Hair_History.jpg',
+    width: 20,
+    height: 14,
+    alt: ''
+  },
   {
     src:
       'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Trisha_on_Black_Couch.jpg',
@@ -207,8 +221,64 @@ const studioPhotos = [
   },
   {
     src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Sherri_Lake.jpg',
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217519/Folck%20Photo/Studio/Angela_Prelude.jpg',
+    width: 15,
+    height: 20,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217519/Folck%20Photo/Studio/Echo_M.jpg',
+    width: 12,
+    height: 16,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Carolyn_in_Silver_Kimono.jpg',
+    width: 10,
+    height: 16,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Geir.jpg',
     width: 18,
+    height: 20,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217519/Folck%20Photo/Studio/Donna_in_Black.jpg',
+    width: 13,
+    height: 20,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217519/Folck%20Photo/Studio/Angelique_Becoming.jpg',
+    width: 8,
+    height: 10,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217519/Folck%20Photo/Studio/1-Corinthians_11_6_k.jpg',
+    width: 8,
+    height: 5,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217519/Folck%20Photo/Studio/Alva_in_White.jpg',
+    width: 16,
+    height: 20,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Butterfly.jpg',
+    width: 16,
     height: 20,
     alt: ''
   },
@@ -221,51 +291,9 @@ const studioPhotos = [
   },
   {
     src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Rachel_with_Hair_History.jpg',
-    width: 20,
-    height: 14,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Geir.jpg',
-    width: 18,
-    height: 20,
-    alt: ''
-  },
-  {
-    src:
       'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Denise_Cat_Grrl.jpg',
     width: 12,
     height: 16,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Isolated.jpg',
-    width: 7,
-    height: 10,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Carolyn_in_Silver_Kimono.jpg',
-    width: 10,
-    height: 16,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Butterfly.jpg',
-    width: 16,
-    height: 20,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Flora_Brown_I.jpg',
-    width: 15,
-    height: 20,
     alt: ''
   },
   {
@@ -291,44 +319,16 @@ const studioPhotos = [
   },
   {
     src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217519/Folck%20Photo/Studio/Donna_in_Black.jpg',
-    width: 13,
-    height: 20,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217519/Folck%20Photo/Studio/Alva_in_White.jpg',
-    width: 16,
-    height: 20,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217519/Folck%20Photo/Studio/Angela_Prelude.jpg',
-    width: 15,
-    height: 20,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217519/Folck%20Photo/Studio/Angelique_Becoming.jpg',
-    width: 8,
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Isolated.jpg',
+    width: 7,
     height: 10,
     alt: ''
   },
   {
     src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217519/Folck%20Photo/Studio/Echo_M.jpg',
-    width: 12,
-    height: 16,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217519/Folck%20Photo/Studio/1-Corinthians_11_6_k.jpg',
-    width: 8,
-    height: 5,
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551217520/Folck%20Photo/Studio/Sherri_Lake.jpg',
+    width: 18,
+    height: 20,
     alt: ''
   }
 ];
@@ -336,51 +336,9 @@ const studioPhotos = [
 const columbariumPhotos = [
   {
     src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551134595/Folck%20Photo/Columbarium/Supplication.jpg',
-    width: 49,
-    height: 32,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551134595/Folck%20Photo/Columbarium/The_Angel_of_Cancer.jpg',
-    width: 20,
-    height: 30,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551134594/Folck%20Photo/Columbarium/Flora_FWC_I.jpg',
-    width: 31,
-    height: 47,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551134594/Folck%20Photo/Columbarium/Chapel_of_Rest_II.jpg',
-    width: 42,
-    height: 53,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551134594/Folck%20Photo/Columbarium/Amy_Reverence.jpg',
-    width: 32,
-    height: 49,
-    alt: ''
-  },
-  {
-    src:
       'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551134594/Folck%20Photo/Columbarium/Belated_Return.jpg',
     width: 30,
     height: 45,
-    alt: ''
-  },
-  {
-    src:
-      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551134594/Folck%20Photo/Columbarium/Only_Begotten_Daughter.jpg',
-    width: 32,
-    height: 49,
     alt: ''
   },
   {
@@ -392,9 +350,53 @@ const columbariumPhotos = [
   },
   {
     src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551134594/Folck%20Photo/Columbarium/Only_Begotten_Daughter.jpg',
+    width: 32,
+    height: 49,
+    alt: ''
+  },
+
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551134594/Folck%20Photo/Columbarium/Chapel_of_Rest_II.jpg',
+    width: 42,
+    height: 53,
+    alt: ''
+  },
+  {
+    src:
       'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551134594/Folck%20Photo/Columbarium/Flora_Providence_I.jpg',
     width: 29,
     height: 45,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551134594/Folck%20Photo/Columbarium/Flora_FWC_I.jpg',
+    width: 31,
+    height: 47,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551134595/Folck%20Photo/Columbarium/The_Angel_of_Cancer.jpg',
+    width: 20,
+    height: 30,
+    alt: ''
+  },
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551134595/Folck%20Photo/Columbarium/Supplication.jpg',
+    width: 49,
+    height: 32,
+    alt: ''
+  },
+
+  {
+    src:
+      'https://res.cloudinary.com/steiger/image/upload/q_auto:eco/v1551134594/Folck%20Photo/Columbarium/Amy_Reverence.jpg',
+    width: 32,
+    height: 49,
     alt: ''
   }
 ];
