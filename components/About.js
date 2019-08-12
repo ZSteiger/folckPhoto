d3e1bc2b7837a9245ad70cfe5e82b4b869706f97
ZSteiger <z.steig93@gmail.com>
2019-08-12 12:37:51 -0400

    Fixed small styling bug

--- components/About.js
+++ components/About.js
@@ -16,7 +16,7 @@ const AboutDiv = styled.div`
     font-size: 1.5rem;
     text-align: center;
   }
-  @media (max-width: 1200px) {
+  @media (max-width: 1000px) {
     grid-template-columns: 1fr;
   }
 `;
