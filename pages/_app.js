36fb3b34877f7ce3c778803e87da5b35956f14ef
ZSteiger <z.steig93@gmail.com>
2019-12-28 14:20:49 -0500

    Updated NextJS / Audited some packages

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,6 @@ import App, { Container } from 'next/app';
 import Page from '../components/Page';
 
 class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
   render() {
     const { Component, pageProps } = this.props;
     return (
